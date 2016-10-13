4be9c5f51eec8cd6a9a3e2551c52e7b168bab499
paste123 <851278439@qq.com>
2016-10-13 22:55:57 +0800

    线程

---
+++ thread.java
@@ -0,0 +1,16 @@
+package Thread1;
+
+public class thread extends Thread {
+
+	public void run(){
+		System.out.println(getName()+"线程跑起来了...");
+		
+		
+	}
+	 
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
